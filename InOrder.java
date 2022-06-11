25d8833a596df7efbbb1b1cbb4896f17d2499d55
Waldis <waldis4u@gmail.com>
2022-06-11 23:49:06 +0100

    Initial commit

---
+++ InOrder.java
@@ -0,0 +1,45 @@
+package inOrder;
+
+
+public class InOrder {
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println("_________________________________________________________________________________________________");
+		System.out.println("|                                                                                                |");
+		System.out.println("|         welcome to Waldis, The best online African unlimited music !!!!!                       |");
+		System.out.println("|                                                                                                |");
+		System.out.println("|________________________________________________________________________________________________|");
+		System.out.println("|                                                                                                |");
+		System.out.println("|                                                                                                |");
+		System.out.println("|                                                                                                |");
+		System.out.println("|       Listen to all local track from : Volta, North, GA, Asante...... Since the 90s!!          |");
+		System.out.println("|          You name it and we have it, BorBorBor, Adowa, Gonja, Dagbeni ..........!!             |");
+		System.out.println("|and we got all the Legends and the gurus too, Dada Lumba, Amakye Dede, Kojo Antwi(mr. music man)|");
+		System.out.println("|                   ##########################################################                   |");
+		System.out.println("|                                                                                                |");
+		System.out.println("|                               Kindly choose your favorite 3 here                               |");
+		System.out.println("|                                                                                                |");
+		System.out.println("|             1, Borborbor                                   2, Adowa                            |");
+		System.out.println("|                                                                                                |");
+		System.out.println("|             3, Gonja                                       4, Dagbani                          |");
+		System.out.println("|                                                                                                |");
+		System.out.println("|             5, Ga                                          6, High Life                        |");
+		System.out.println("|                                                                                                |");
+		System.out.println("|             7, Hip Life                                    8, Gospel                           |");
+		System.out.println("|                                                                                                |");
+		System.out.println("|             9, Reggea                                     10, 90s - 00s!!!                     |");
+		System.out.println("|                                                                                                |");
+		System.out.println("|            11, Adadamu                                    12, Latest Hitz                      |");
+		System.out.println("|                                                                                                |");
+		System.out.println("|                              WALDIS App..............!!!!!                                     |");
+		System.out.println("|                                                                                                |");
+		System.out.println("|                                                                                                |");
+		System.out.println("|________________________________________________________________________________________________|");
+		
+		
+		
+	}
+
+}
